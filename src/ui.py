08d2a69741d0d01357015ded07f9e42c4540b8a7
adiorinder <adith.prakashinder@gmail.com>
2026-01-16 00:01:52 +0530

    v0.91
    Added
    * App Icon
    * Multiple Days
    * Warnings
    * Native Language Support
    * Color Encoding

--- src/ui.py
+++ src/ui.py
@@ -17,36 +17,90 @@ LabelBase.register(
 Builder.load_file("src/ui.kv")
 
 weather_data = {
+  "location": {
+    "lat": 24.8039,
+    "lon": 93.9420,
+    "district": "Imphal East"
+  },
+
+  "timestamp": "2026-01-15T22:30:45+05:30",
+
+  "observed_weather": {
+    "temperature_c": 32.4,
+    "pressure_hpa": 1006,
+    "humidity_percent": 71,
+    "wind_speed_mps": 3.2
+  },
     "today": {
-        "tempMax": 32,
-        "tempMin": 28,
-        "rain": 150,
+        "temp_max": 32,
+        "temp_min": 28,
+        "rainfall_mm": 150,
         "humidity": 65,
         "wind_speed": 14
     },
     "tomorrow": {
-        "tempMax": 30,
-        "tempMin": 25,
-        "rain": 120,
+        "temp_max": 30,
+        "temp_min": 25,
+        "rainfall_mm": 120,
         "humidity": 58,
         "wind_speed": 10
     },
     "day_after": {
-        "tempMax": 29,
-        "tempMin": 24,
-        "rain": 134,
+        "temp_max": 29,
+        "temp_min": 24,
+        "rainfall_mm": 134,
         "humidity": 70,
         "wind_speed": 18
+    },
+    
+  
+  "weather_prediction": {
+    "rainfall_mm": 78.4
+  },
+
+  "warnings": {
+    "heatwave": {
+      "severity": "NONE",
+      "reason": "Conditions not met"
+    },
+    "coldwave": {
+      "severity": "NONE",
+      "reason": "Conditions not met"
+    },
+    "thunderstorm": {
+      "severity": "MODERATE",
+      "reason": "High moisture, cloud buildup and pressure drop"
+    },
+    "hailstorm": {
+      "severity": "NONE",
+      "reason": "No hail-favorable dynamics"
     }
+  },
+
+  "risk_assessment": {
+    "flood": {
+      "probability": 0.82,
+      "risk_level": "HIGH"
+    },
+    "landslide": {
+      "probability": 0.61,
+      "risk_level": "MODERATE"
+    },
+    "earthquake": {
+      "zone": "HIGH"
+    }
+  },
+
+  "overall_alert": "SEVERE"
 }
 
 class WeatherUI(BoxLayout):
-    today_tempMax = NumericProperty(0)
-    today_tempMin = NumericProperty(0)
-    tomorrow_tempMax = NumericProperty(0)
-    tomorrow_tempMin = NumericProperty(0)
-    day_after_tempMax = NumericProperty(0)
-    day_after_tempMin = NumericProperty(0)
+    today_temp_max = NumericProperty(0)
+    today_temp_min = NumericProperty(0)
+    tomorrow_temp_max = NumericProperty(0)
+    tomorrow_temp_min = NumericProperty(0)
+    day_after_temp_max = NumericProperty(0)
+    day_after_temp_min = NumericProperty(0)
     # change this for integer
     today_rain = NumericProperty(0)
     tomorrow_rain = NumericProperty(0)
@@ -59,16 +113,20 @@ class WeatherUI(BoxLayout):
             "title": "Sangai Weather",
             "forecast": "Weather Forecast",
             "today": "TODAY",
+            "tomorrow": "Tomorrow",
+            "day_after":"Day After Tomorrow",
             "temp": "Temperature",
-            "rain": "Rainfall",
+            "rainfall_mm": "Rainfall",
             "more": "More Info"
         },
         "mni": {    
             "title": "ꯁꯥꯡꯒꯥꯏ ꯋꯦꯗꯔ",
             "forecast": "ꯋꯦꯗꯔ ꯐꯣꯔꯀꯥꯁ꯭ꯇ",
             "today": "ꯅꯨꯡꯁꯤ",
+            "tomorrow": "ꯅꯤꯡꯊꯥꯡ",
+            "day_after": "ꯂꯩꯡꯊꯥꯡ",
             "temp": "ꯍꯤꯟꯅꯩ",
-            "rain": "ꯂꯩꯔꯥꯟ",
+            "rainfall_mm": "ꯂꯩꯔꯥꯟ",
             "more": "ꯍꯣꯠꯅ ꯑꯃꯇ"
         }
     }
@@ -83,18 +141,59 @@ class WeatherUI(BoxLayout):
         return weather_data
     
     def set_data(self):
-        self.today_tempMax = self.get_data()["today"]["tempMax"]
-        self.today_tempMin = self.get_data()["today"]["tempMin"]
-        self.today_rain = self.get_data()["today"]["rain"]
+        self.today_temp_max = self.get_data()["today"]["temp_max"]
+        self.today_temp_min = self.get_data()["today"]["temp_min"]
+        self.today_rainfall_mm = self.get_data()["today"]["rainfall_mm"]
+        self.tomorrow_temp_max = self.get_data()["tomorrow"]["temp_max"]
+        self.tomorrow_temp_min = self.get_data()["tomorrow"]["temp_min"]
+        self.tomorrow_rainfall_mm = self.get_data()["tomorrow"]["rainfall_mm"]    
+        self.day_after_temp_max = self.get_data()["day_after"]["temp_max"]
+        self.day_after_temp_min = self.get_data()["day_after"]["temp_min"]
+        self.day_after_rainfall_mm = self.get_data()["day_after"]["rainfall_mm"]
     
     
     def send_location(self):
         pass
 
+    def check_warnings(self):
+        warnings = self.get_data()["warnings"]
 
+        for event, info in warnings.items():
+            if info["severity"] != "NONE":
+
+                msg = (
+                    f"{event.upper()}\n\n"
+                    f"Severity: {info['severity']}\n"
+                    f"Reason: {info['reason']}"
+                )
+
+                self.show_popup(msg)
+    def show_popup(self, text):
+
+        lbl = Label(
+            text=text,
+            halign="center",
+            valign="middle",
+            text_size=(280, None), 
+            size_hint=(None, None),
+            size=(340, 200)
+        )
+        layout = BoxLayout(
+            padding=[15, 15, 15, 20]  
+        )
+        layout.add_widget(lbl)
+        popup = Popup(
+            title="⚠ Weather Alert",
+            content=layout,
+            size_hint=(None, None),
+            size=(420, 400),
+            auto_dismiss=True
+        )
+
+        popup.open()
+
+    
     def show_details(self, day):
-        
-
         text = (
             f"Humidity: {self.get_data()[day]['humidity']}%\n"
             f"Wind Speed: {self.get_data()[day]['wind_speed']} km/h\n"
@@ -111,6 +210,7 @@ class WeatherApp(App):
     def build(self):
         return WeatherUI()
     def on_start(self):
+        self.root.check_warnings()
         self.root.set_data()
 
 
