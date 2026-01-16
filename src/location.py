f10ae7e3d918522767e7f7c40daad56f7c3e2114
Mahima-gour <69231106+Mahima-gour@users.noreply.github.com>
2026-01-16 14:00:33 +0530

    v1.0

--- src/location.py
+++ src/location.py
@@ -3,6 +3,5 @@ import requests
 def get_loc():
     data = requests.get("https://ipinfo.io/json").json()
     lat, lon = data['loc'].split(",")
-    print(lat, lon)
-    return 22,77
+    return (lat, lon)
 get_loc()
